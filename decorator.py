0772213de86a3a8b1c8d8e4301955dbb349bc885
Anderson Luiz <andersonn.negocios@gmail.com>
2025-01-20 17:50:38 -0300

    exercicio unir listas e decorators

--- decorator.py
+++ decorator.py
@@ -1,4 +1,4 @@
-﻿#FUNÇÃO DECORADORA
+﻿#FUNÇÃO DECORADORA FEITO SEM USAR O @
 def criar_funcao(fun):
     def interna(*args, **kwargs):
         print('Vou te decorar!')
@@ -42,6 +42,62 @@ def teste(*args, **kwargs):
 
 v1 = teste('Feijão', valor=12)
 print(v1)
+
+print()
+print()
+
+#OUTRO EXEMPLO DE DECORATOR
+def decorator(func):
+    print('Decorador 1')
+
+    def aninhada(*args, **kwargs):
+        print('Aninhada')
+        res = func(*args, **kwargs)
+        return res
+    return aninhada
+
+def soma(x,y):
+    return x + y
+
+multiplica = decorator(lambda x, y: x * y)
+
+v2 = soma(10,20)
+v3 = multiplica(10, 5)
+print(v2)
+print(v3)
+
+print()
+print()
+
+# Unindo Listas com função ZIP
+# def zipper(list1, list2):
+#     new_list = list(zip(list1,list2))
+#     return new_list
+
+def zipper(l1, l2):
+    new_list = min(len(l1), len(l2))
+    return [(l1[i], l2[i]) for i in range(new_list)]
+
+
+list1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+
+list2 = ['BA', 'MG', 'SP', 'RJ']
+
+new = zipper(list1,list2)
+print(new)
+
+
+#TESTANDO FUNÇÃO MAX
+n1 = "Anderson"
+n2 = "luiz"
+max_list = max(len(n1), len(n2))
+print(max_list)
+
+
+
+
+
+
 
 
 
---
+++ exercicio_somar_listas.py
@@ -0,0 +1,53 @@
+﻿
+def soma_listas(l1,l2):
+    soma = []
+    for n1, n2 in zip(l1,l2):
+        soma.append(n1 + n2)
+    return soma
+
+
+list_1 = [1,2,3,4,5,6,7]
+list_2 = [1,2,3,4,]
+
+lista_somada = soma_listas(list_1,list_2)
+print()
+print("Método com função e zip")
+print(list_1)
+print(list_2)
+print(lista_somada)
+
+print()
+print()
+
+#FORMA COM LOOP pode ser troca o range..
+# e len pelo enumerate e adicionar uma variavel no loop
+listA1 = [1,2,3,4,5,6,7]
+listA2 = [1,2,3,4,]
+
+lista_soma = []
+for i in range(len(listA2)):
+    lista_soma.append(listA1[i]+listA2[i])
+print("Método com looping")
+print(listA1)
+print(listA2)
+print(lista_soma)
+
+#Método com list comprehension com zip_longest (captura a lista maior)
+from itertools import zip_longest
+list_a = [1,2,3,4,5,6,7]
+list_b = [1,2,3,4,]
+soma_lista = [x + y for x, y in zip_longest(list_a,list_b, fillvalue=0)]
+print()
+print()
+print("Método com list comprehension")
+print(list_a)
+print(list_b)
+print(soma_lista)
+
+
+
+
+
+
+
+
