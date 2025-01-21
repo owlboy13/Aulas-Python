cd61c11c2fcb9b493a1742d07f5fbc3e5c2cf6bf
Anderson Luiz <andersonn.negocios@gmail.com>
2025-01-21 19:43:48 -0300

    combinantions

---
+++ combinations.py
@@ -0,0 +1,35 @@
+﻿# Combinatios, Permutations e Product - itertools
+
+from itertools import combinations, permutations, product
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Leticia',
+]
+
+camisetas = [
+    ['preta', 'branca'],
+    ['p','m',],
+    ['masculino','feminino','unissex'],
+    ['algodão','poliéster']
+]
+
+# chama a lista e o insere o numero de grupos 
+# que deseja combinar
+new_list = list(combinations(pessoas, 2))
+list_perm = list(permutations(pessoas, 2))
+# precisa desempacotar para combinar os produtos
+lista_camisas = list(product(*camisetas)) 
+
+for i in new_list:
+    print(i)
+
+print()
+
+for r in list_perm:
+    print(r)
+
+print()
+
+for c in lista_camisas:
+    print(c)
+
---
+++ count.py
@@ -0,0 +1,19 @@
+﻿#COUNT É UM ITERADOR SEM FIM (itertools)
+from itertools import count
+
+
+c1 = count(start=8, step=8) # com arguentos para soma
+r1 = range(10)
+
+#verificar se o metodo é iterator
+print( 'c1', hasattr(c1, '__iter__'))
+
+for i in c1:
+    if i >= 100:
+        break
+    print(i)
+print()
+
+for r in r1:
+    print(r)
+
